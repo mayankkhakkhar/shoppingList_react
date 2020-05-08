5c48438a89dd976f058f9f8f7a8721311bf7ce13
mayankkhakkhar <grandmastermayank@gmail.com>
2020-05-08 16:25:17 +0530

    Updates

---
+++ precache-manifest.26454070c90aa1c3934197132ddfe0be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9662d91160302a4bdb95c4566ac144",
+    "url": "/shoppingList_react/index.html"
+  },
+  {
+    "revision": "1978cc39137885b6510f",
+    "url": "/shoppingList_react/static/css/2.21650121.chunk.css"
+  },
+  {
+    "revision": "76a3c4592405033b9b37",
+    "url": "/shoppingList_react/static/css/main.13aee61c.chunk.css"
+  },
+  {
+    "revision": "1978cc39137885b6510f",
+    "url": "/shoppingList_react/static/js/2.54a92e63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoppingList_react/static/js/2.54a92e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76a3c4592405033b9b37",
+    "url": "/shoppingList_react/static/js/main.d419e89d.chunk.js"
+  },
+  {
+    "revision": "8c0dc0d3c79d023106ba",
+    "url": "/shoppingList_react/static/js/runtime-main.0f1c0f18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shoppingList_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
